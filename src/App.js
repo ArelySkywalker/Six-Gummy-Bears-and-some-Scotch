1e8cf74092e414b1dc1c07aff05d229408cc8f20
Arely Miramontes Rodriguez <arelymts@gmail.com>
2019-08-05 17:58:36 -0500

    add SaSS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Main from './Components/Main';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import './Styles/App.scss';
 
 function App() {
 	return (
