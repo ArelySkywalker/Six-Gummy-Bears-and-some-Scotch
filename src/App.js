c85faa6c254f056bf1f91a3d553261978f54421f
Arely Miramontes Rodriguez <arelymts@gmail.com>
2019-08-20 23:14:31 -0500

    fix mobile scroll

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import MainContainer from './Containers/MainContainer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Styles/App.scss';
 
+var is_root = window.location.pathname == "/";
+
 function App() {
 	return (
 		<div className="App">
-			<div className="Main">
+			<div className={`Main ${ is_root ? `root` : `` }`}>
 				<Header />
 				<MainContainer />
 			</div>
